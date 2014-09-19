127d0d5550044adc00cf6103f9674615633b4006
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-09-19 12:11:10 -0300

    Capitulo 08 - Atividade 02 - Bean de usuario logado

---
+++ src/main/java/br/com/caelum/vraptor/bean/UsuarioLogado.java
@@ -0,0 +1,27 @@
+package br.com.caelum.vraptor.bean;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import br.com.caelum.vraptor.model.Usuario;
+
+@Named
+@SessionScoped
+public class UsuarioLogado implements Serializable{
+
+	private static final long serialVersionUID = -1687898686784376042L;
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+}
